23f9dde25db2e7840fb624caebfef7bcf4ba9dc3
Stephen Gacio <sgacio@mail.usf.edu>
2019-08-13 16:30:05 -0400

    explorer mode complete

--- public/main.js
+++ public/main.js
@@ -1,7 +1,24 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let city = document.querySelector('.typed')
+
+const createProperSearch = () => {
+  let typedName = city.value
+  const url = 'http://api.openweathermap.org/data/2.5/weather'
+  let search = typedName
+  const apiKey = '98f2e44fc966d04df9c6296c9f342706'
+  const newUrl = url + '?' + 'q=' + search + '&units=imperial' + '&appid=' + apiKey
+  return newUrl
+}
+const getWeatherAndTemp = () => {
+  fetch(createProperSearch())
+    .then(response => {
+      return response.json()
+    })
+    .then(city => {
+      console.log(city)
+      document.querySelector('.display-weather').textContent = city.weather[0].main
+      document.querySelector('.display-temp').textContent = city.main.temp
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+// document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.search').addEventListener('click', getWeatherAndTemp)
